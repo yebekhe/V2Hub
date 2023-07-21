33fa6e580e4b23175082bdd6fe453fee2d3681b9
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-22 01:30:36 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+
+$subscription_urls = [
+  "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
+  "http://176.124.198.154/subscribe.txt",
+  "http://5.230.67.53/subscribe.txt",
+  
+];
