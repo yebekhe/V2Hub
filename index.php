9e0441394525d46456502f7bb3b7faa5afa94653
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-22 01:34:14 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?
+
+require "config.php";
+require "functions.php";
+
+$merged_data = merge_subscription($subscription_urls);
+
+file_put_contents("merged", $merged_data);
+file_put_contents("merged_base64", base64_encode($merged_data));
