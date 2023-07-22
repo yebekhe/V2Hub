860b9f3b15aac35a56767103fd2bafcb0bf75192
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-22 15:37:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,23 @@ include "config.php";
 include "functions.php";
 
 $merged_data = merge_subscription($subscription_urls);
-$splited_by_protocol = split_by_protocol($merged_data);
+$merged_vmess = array_to_subscription($merged_data['vmess']) . "\n";
+$merged_vless = array_to_subscription($merged_data['vless']) . "\n";
+$merged_reality = get_reality($merged_vless);
+$merged_trojan = array_to_subscription($merged_data['trojan']) . "\n";
+$merged_shadowsocks = array_to_subscription($merged_data['ss']) . "\n";
+$merged_mix = $merged_vmess . $merged_vless . $merged_trojan . $merged_shadowsocks ;
 
-file_put_contents("merged", $merged_data);
-file_put_contents("merged_base64", base64_encode($merged_data));
+file_put_contents("merged", $merged_mix);
+file_put_contents("merged_base64", base64_encode($merged_mix));
 
-file_put_contents("Split/Normal/vmess", array_to_subscription($splited_by_protocol['vmess']));
-file_put_contents("Split/Base64/vmess", base64_encode(array_to_subscription($splited_by_protocol['vmess'])));
-file_put_contents("Split/Normal/vless", array_to_subscription($splited_by_protocol['vless']));
-file_put_contents("Split/Base64/vless", base64_encode(array_to_subscription($splited_by_protocol['vless'])));
-file_put_contents("Split/Normal/reality", get_reality(array_to_subscription($splited_by_protocol['vless'])));
-file_put_contents("Split/Base64/reality", base64_encode(get_reality(array_to_subscription($splited_by_protocol['vless']))));
-file_put_contents("Split/Normal/trojan", array_to_subscription($splited_by_protocol['trojan']));
-file_put_contents("Split/Base64/trojan", base64_encode(array_to_subscription($splited_by_protocol['trojan'])));
-file_put_contents("Split/Normal/shadowsocks", array_to_subscription($splited_by_protocol['ss']));
-file_put_contents("Split/Base64/shadowsocks", base64_encode(array_to_subscription($splited_by_protocol['ss'])));
+file_put_contents("Split/Normal/vmess", $merged_vmess);
+file_put_contents("Split/Base64/vmess", base64_encode($merged_vmess));
+file_put_contents("Split/Normal/vless", $merged_vless);
+file_put_contents("Split/Base64/vless", base64_encode($merged_vless));
+file_put_contents("Split/Normal/reality", $merged_reality);
+file_put_contents("Split/Base64/reality", base64_encode($merged_reality));
+file_put_contents("Split/Normal/trojan", $merged_trojan);
+file_put_contents("Split/Base64/trojan", base64_encode($merged_trojan));
+file_put_contents("Split/Normal/shadowsocks", $merged_shadowsocks);
+file_put_contents("Split/Base64/shadowsocks", base64_encode($merged_shadowsocks));
