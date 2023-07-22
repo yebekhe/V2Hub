7cbcd0415fde554daa8be281176eb3e131dfd688
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-22 20:40:01 +0330

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,5 @@ $subscription_urls = [
   "https://amghezi.duckdns.org/zaku68HqhhMq/93ed0514-854b-4e6c-9967-f8dc4b40db8a/all.txt?name=drctsublink-unknown&asn=unknown&mode=new",
   "https://nika4.hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=direct-unknown&asn=unknown&mode=new",
   "https://nika3hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=cdnall-unknown&asn=unknown&mode=new",
-  "https://raw.githubusercontent.com/LonUp/NodeList/main/V2RAY/Latest.txt?s=09",
   "https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config",
 ];
