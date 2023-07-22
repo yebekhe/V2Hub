ba63ada41bd144d1ee407dc2e8aebdb619e2a5ce
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-07-22 15:07:43 +0330

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ file_put_contents("Split/Normal/vmess", array_to_subscription($splited_by_protoc
 file_put_contents("Split/Base64/vmess", array_to_subscription(base64_encode($splited_by_protocol['vmess'])));
 file_put_contents("Split/Normal/vless", array_to_subscription($splited_by_protocol['vless']));
 file_put_contents("Split/Base64/vless", array_to_subscription(base64_encode($splited_by_protocol['vless'])));
+file_put_contents("Split/Normal/reality", get_reality(array_to_subscription($splited_by_protocol['vless'])));
+file_put_contents("Split/Base64/reality", get_reality(array_to_subscription(base64_encode($splited_by_protocol['vless']))));
 file_put_contents("Split/Normal/trojan", array_to_subscription($splited_by_protocol['trojan']));
 file_put_contents("Split/Base64/trojan", array_to_subscription(base64_encode($splited_by_protocol['trojan'])));
 file_put_contents("Split/Normal/shadowsocks", array_to_subscription($splited_by_protocol['ss']));
