3d2a31478f2ebf5f05f20d716a4b1c7b38d7499a
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-09-08 00:22:58 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 
 $subscription_urls = [
-  "https://alienvpn402.github.io/AlienVPN402-subscribe-servers/",
+  "https://raw.githubusercontent.com/AlienVPN402/AlienVPN402-subscribe-servers-sing-box/main/index.html",
   "https://branch.blanku.me/",
-  "https://raw.githubusercontent.com/irancpi/Subscription/main/Sub",
-  "https://raw.githubusercontent.com/amirmohammad-mohammad-88/Sub-Reality-Azadi-config/Config/Azadi-Reality-Different",
-  "https://sublink.cpionline.co/zaku68HqhhMq/93ed0514-854b-4e6c-9967-f8dc4b40db8a/all.txt?name=sublink-MCI&asn=MCI&mode=new",
-  "https://amghezi.duckdns.org/zaku68HqhhMq/93ed0514-854b-4e6c-9967-f8dc4b40db8a/all.txt?name=drctsublink-unknown&asn=unknown&mode=new",
-  "https://nika4.hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=direct-unknown&asn=unknown&mode=new",
-  "https://nika3hdfy2.foriran.trade/sqjC9UQotFfQNr6bHWuKrJ/79770a32-9607-4919-9483-0f1794559390/all.txt?name=cdnall-unknown&asn=unknown&mode=new",
   "https://raw.githubusercontent.com/IranianCypherpunks/sub/main/config",
+  "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
+  "https://raw.githubusercontent.com/mianfeifq/share/main/data2023094.txt",
+  "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
+  "https://raw.githubusercontent.com/freefq/free/master/v2",
+  "https://test2.sub-channel.workers.dev/"
 ];
